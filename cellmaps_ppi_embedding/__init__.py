e735e53114b2cf65f061a20021412f6a83fd5ef9
Chris Churas <churas.camera@gmail.com>
2024-02-01 11:39:34 -0800

    set release to today, removed alpha from version

--- cellmaps_ppi_embedding/__init__.py
+++ cellmaps_ppi_embedding/__init__.py
@@ -2,9 +2,9 @@
 
 """Top-level package for cellmaps_ppi_embedding."""
 
-__author__ = 'Ideker Lab Cell Maps team'
+__author__ = 'Cell Maps team'
 __email__ = 'tools@cm4ai.org'
-__version__ = '0.1.0a9'
+__version__ = '0.1.0'
 __repo_url__ = 'https://github.com/idekerlab/cellmaps_ppi_embedding'
 __description__ = 'A tool to generate embeddings from networks'
 __computation_name__ = 'AP-MS Embedding'
