d481229b737ad1c3398fffbee72feb301514e9e6
dzoanka <joanna.lenkiewicz@gmail.com>
2024-12-02 13:51:59 +0100

    Drop alpha

--- cellmaps_ppi_embedding/__init__.py
+++ cellmaps_ppi_embedding/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = 'Cell Maps team'
 __email__ = 'tools@cm4ai.org'
-__version__ = '0.4.0a1'
+__version__ = '0.4.0'
 __repo_url__ = 'https://github.com/idekerlab/cellmaps_ppi_embedding'
 __description__ = 'A tool to generate embeddings from networks'
 __computation_name__ = 'AP-MS Embedding'
